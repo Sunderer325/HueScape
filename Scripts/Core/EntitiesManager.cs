161fe807483ee3595b777156daf81a769e8e08dd
andrey <sundererjob@gmail.com>
2020-11-26 20:40:43 +0500

    organized

---
+++ Scripts/Core/EntitiesManager.cs
@@ -0,0 +1,250 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityUlilities;
+
+public class EntitiesManager : MonoBehaviour
+{
+	public GameObject box;
+	public GameObject stack;
+	public GameObject paintWall;
+	public GameObject player;
+	public GameObject bonus;
+	public GameObject stars;
+	public GameObject indicator;
+
+	List<GameObject> stacks = new List<GameObject>();
+
+	GameManager game;
+	GameViewManager viewManager;
+	DifficultyManager difficulties;
+	PlayerSkinManager skinManager;
+	AudioManager audioManager;
+	SettingsManager settingsManager;
+	bool isFirst = true;
+
+	bool bwMode = true;
+	HSBColor color, newColor;
+	HSBColor remindColor;
+	FloatRange bonusSpeed = new FloatRange(2.0f, 3.0f);
+
+	public void Init()
+	{
+		game = GetComponent<GameManager>();
+		viewManager = GetComponent<GameViewManager>();
+		difficulties = GetComponent<DifficultyManager>();
+		skinManager = FindObjectOfType<PlayerSkinManager>();
+		viewManager.OnInvertColors = OnInvertColors;
+		audioManager = FindObjectOfType<AudioManager>();
+		settingsManager = FindObjectOfType<SettingsManager>();
+	}
+	public int StacksCount => stacks.Count;
+	public void SetColor(HSBColor _color) { color = _color; }
+	public void SpawnPlayer()
+	{
+		player = Instantiate(player);
+		Player pl = player.GetComponent<Player>();
+		pl.Init();
+		pl.SetSkin(skinManager.GetSkin());
+		pl.OnDie += OnPlayerDie;
+		pl.OnAddPoint += OnAddPoint;
+		pl.OnBonusTake += OnBonusTake;
+		pl.OnEnterBox += OnPlayerEnterToBox;
+		pl.OnPaintWallEnter += OnPlayerEnterToPaintWall;
+	}
+	public void SpawnStack()
+	{
+		stacks.Add(Instantiate(stack));
+		stacks[stacks.Count - 1].GetComponent<Stack>().Init(box, color, difficulties.StackSpeed, difficulties.ColorGap, difficulties.AmountOfBoxes);
+		
+		if (isFirst)
+		{
+			stacks[0].GetComponent<Stack>().onDestroy += OnStackDestroy;
+			isFirst = false;
+		}
+	}
+	public bool SpawnPaintWall(HSBColor _color)
+	{
+		newColor = _color;
+		if (stacks.Count == 1)
+		{
+			Stack otherColorStack = stacks[0].GetComponent<Stack>();
+			if (otherColorStack.GetLowestPosition().y - player.transform.position.y > 3.0f)
+			{
+				Instantiate(paintWall).GetComponent<PaintWall>().Init(CalculatePaintWallSpeed(otherColorStack), newColor);
+				SetColor(newColor);
+				return false;
+			}
+		}
+		else if (stacks.Count > 1)
+		{
+			Stack colorStack = stacks[stacks.Count - 2].GetComponent<Stack>();
+			Stack otherColorStack = stacks[stacks.Count - 1].GetComponent<Stack>();
+
+			if (colorStack.GetHighestPosition().y < player.transform.position.y)
+				Instantiate(paintWall).GetComponent<PaintWall>().Init(CalculatePaintWallSpeed(otherColorStack), newColor);
+			else
+				Instantiate(paintWall).GetComponent<PaintWall>().Init(CalculatePaintWallSpeed(colorStack, otherColorStack), newColor);
+			SetColor(newColor);
+			return false;
+		}
+		return true;
+	}
+	private float CalculatePaintWallSpeed(Stack otherColorStack)
+	{
+		float distanceFromStackToPlayer = otherColorStack.GetLowestPosition().y - player.transform.position.y;
+		float timeFromStackToPlayer = distanceFromStackToPlayer / otherColorStack.GetSpeed;
+		float distanceFromSpawnToPlayer = ((ScreenHelper.screenHeight / 2) + 0.5f) - player.transform.position.y;
+		float paintWallSpeed = distanceFromSpawnToPlayer / (timeFromStackToPlayer / 1.5f);
+		return paintWallSpeed;
+	}
+	private float CalculatePaintWallSpeed(Stack colorStack, Stack otherColorStack)
+	{
+		float distanceFromColorStackToPlayer = colorStack.GetLowestPosition().y - player.transform.position.y;
+		float timeFromColorStackToPlayer = distanceFromColorStackToPlayer / colorStack.GetSpeed;
+
+		float distanceFromOtherColorStackToPlayer = otherColorStack.GetLowestPosition().y - player.transform.position.y;
+		float timeFromOtherColorStackToPlayer = distanceFromOtherColorStackToPlayer / otherColorStack.GetSpeed;
+
+		float distanceFromSpawnToPlayer = (ScreenHelper.screenHeight / 2) - player.transform.position.y;
+		float paintWallTime = timeFromOtherColorStackToPlayer - (timeFromOtherColorStackToPlayer - timeFromColorStackToPlayer) / 2;
+		float paintWallSpeed = distanceFromSpawnToPlayer / paintWallTime;
+		return paintWallSpeed;
+	}
+
+	public void SpawnBonus()
+	{
+		Instantiate(bonus).GetComponent<Bonus>().Init(bonusSpeed.Random, (BonusType)new IntRange(0, 2).Random); 
+	}
+	public void DestroyAllGOs()
+	{
+		while (stacks.Count > 0)
+		{
+			stacks[0].GetComponent<Stack>().DestroyStack();
+		}
+		Destroy(player);
+	}
+	public void BlackAndWhiteModeInit()
+	{
+		audioManager.ChangeMusicPitch(0.5f);
+		remindColor = color;
+		Camera.main.backgroundColor = new Color(0.9f, 0.9f, 0.9f, 1f);
+		stars.GetComponent<ParticleSystemRenderer>().material.SetColor("_Color", Color.black);
+		player.GetComponent<Player>().SetColor(HSBColor.FromRGB(Color.black));
+		SetColor(HSBColor.FromRGB(Color.black));
+
+		foreach (GameObject go in stacks)
+			go.GetComponent<Stack>().PaintToBlackAndWhite();
+		stacks[0].GetComponent<Stack>().DestroyStack();
+	}
+	public bool BlackAndWhiteModeEnd()
+	{
+		if(stacks.Count == 0)
+		{
+			audioManager.ChangeMusicPitch(1.0f);
+			Camera.main.backgroundColor = new Color(0.14f, 0.14f, 0.14f, 1f);
+			stars.GetComponent<ParticleSystemRenderer>().material.SetColor("_Color", Color.white);
+			player.GetComponent<Player>().SetColor(remindColor);
+			SetColor(remindColor);
+			return true;
+		}
+		return false;
+	}
+	public void ColorBoostModeInit()
+	{
+		player.GetComponent<Player>().ColorBoostMode();
+		audioManager.ChangeMusicPitch(3f);
+	}
+	public bool ColorBoostModeEnd()
+	{
+		if(stacks.Count == 0)
+		{
+			player.GetComponent<Player>().ColorBoostMode();
+			audioManager.ChangeMusicPitch(1.0f);
+			return true;
+		}
+		return false;
+	}
+
+	private void OnPlayerEnterToPaintWall()
+	{
+		audioManager.Play(ClipType.PaintWall);
+		if(settingsManager.vibrationFlag)
+			Vibration.Vibrate(new long[] { 100, 60}, -1);
+	}
+	private void OnInvertColors()
+	{
+		if (bwMode)
+		{
+			BlackAndWhiteModeInit();
+			bwMode = false;
+		}
+		else
+		{
+			bwMode = true;
+		}
+	}
+	private void OnBonusTake(BonusType type, GameObject bonus)
+	{
+		if (settingsManager.vibrationFlag)
+			Vibration.VibratePeek();
+		if (type == BonusType.BlackAndWhite)
+		{
+			game.ChangeGameMode(GameModes.BlackAndWhite);
+		}
+		else if (type == BonusType.ColorBoost)
+		{
+			game.ChangeGameMode(GameModes.ColorBoost);
+		}
+		else if(type == BonusType.AddPoints)
+		{
+			audioManager.Play(ClipType.Buy);
+			game.OnAddPoints(Bonus.BonusPoints);
+		}
+
+	}
+	private void OnPlayerDie()
+	{
+		if (settingsManager.vibrationFlag)
+			Vibration.VibratePeek();
+		audioManager.Play(ClipType.Death);
+		game.OnGameOver();
+		stars.GetComponent<ParticleSystem>().Clear();
+		Instantiate(indicator, player.transform.position, Quaternion.identity).GetComponent<Indicator>().InitError();
+	}
+	private void OnAddPoint()
+	{
+		game.OnAddPoint();
+	}
+	private void OnPlayerEnterToBox()
+	{
+		if(game.GameMode == GameModes.Normal){
+			audioManager.Play(ClipType.Stack);
+			Instantiate(indicator, player.transform.position, Quaternion.identity).GetComponent<Indicator>().InitWhite(stacks[0].GetComponent<Stack>().GetSpeed);
+			if (settingsManager.vibrationFlag)
+				Vibration.Vibrate(new long[] {100, 30 }, -1);
+		}
+		else if(game.GameMode == GameModes.BlackAndWhite){
+			audioManager.Play(ClipType.StackReverb);
+			Instantiate(indicator, player.transform.position, Quaternion.identity).GetComponent<Indicator>().InitBlack(stacks[0].GetComponent<Stack>().GetSpeed);
+			if (settingsManager.vibrationFlag)
+				Vibration.Vibrate(new long[] { 100, 30 }, -1);
+		}
+		else if(game.GameMode == GameModes.ColorBoost){
+			audioManager.Play(ClipType.Stack);
+			Instantiate(indicator, player.transform.position, Quaternion.identity).GetComponent<Indicator>().InitBlack(stacks[0].GetComponent<Stack>().GetSpeed);
+		}
+	}
+	private void OnStackDestroy()
+	{
+		stacks[0].GetComponent<Stack>().onDestroy -= OnStackDestroy;
+
+		stacks.RemoveAt(0);
+
+		if (stacks.Count > 0)
+		{
+			stacks[0].GetComponent<Stack>().onDestroy += OnStackDestroy;
+		}
+		else isFirst = true;
+	}
+}
---
+++ Scripts/Entities/Stack.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityUlilities;
+
+public class Stack : MonoBehaviour
+{
+    GameObject box;
+    float speed;
+    float boxScale;
+    int amountOfBoxes;
+    List<GameObject> stack = new List<GameObject>();
+
+    HSBColor color;
+    float colorGap;
+    int colorPosition;
+
+    public Action onDestroy;
+
+    #region Public Properties
+    public HSBColor GetColor => color;
+    public float GetSpeed => speed; 
+    public void Init(GameObject _box, HSBColor _color, float _speed, float _colorGap, int _amountOfBoxes)
+    {
+        box = _box;
+        color = _color;
+        speed = _speed;
+        colorGap = _colorGap;
+        amountOfBoxes = _amountOfBoxes;
+        colorPosition = new IntRange(0, amountOfBoxes - 1).Random;
+        MakeStack();
+    }
+    public void SetColor(HSBColor _color)
+    {
+        color = _color;
+        HSBColor baseColor = new HSBColor(color.h - colorGap * colorPosition, color.s, color.b);
+
+        for (int i = 0; i < amountOfBoxes; i++)
+        {
+            baseColor = ClampColor(baseColor);
+
+            stack[i].GetComponent<Box>().SetColor(baseColor);
+
+            baseColor = new HSBColor(baseColor.h + colorGap, baseColor.s, baseColor.b);
+        }
+    }
+    public Vector3 GetLowestPosition()
+    {
+        Vector3 pos = new Vector3(0, ScreenHelper.screenHeight / 2, 0);
+        foreach (GameObject box in stack)
+        {
+            if (box.transform.position.y < pos.y)
+                pos = box.transform.position;
+        }
+        Vector3 size = box.GetComponent<Renderer>().bounds.size;
+        pos = new Vector3(pos.x, pos.y - size.y/2, pos.z);
+        return pos;
+    }
+    public Vector3 GetHighestPosition()
+    {
+        Vector3 pos = new Vector3(0, -ScreenHelper.screenHeight/2, 0);
+        foreach (GameObject box in stack)
+        {
+            if (box.transform.position.y > pos.y)
+                pos = box.transform.position;
+        }
+        Vector3 size = box.GetComponent<Renderer>().bounds.size;
+        pos = new Vector3(pos.x, pos.y + size.y/2, pos.z);
+        return pos;
+    }
+    public void DestroyStack()
+    {
+        foreach (GameObject go in stack)
+            Destroy(go);
+
+        onDestroy();
+        Destroy(gameObject);
+    }
+
+    public void PaintToBlackAndWhite()
+    {
+        foreach(GameObject go in stack)
+        {
+            if (go.GetComponent<Box>().GetColor == color)
+            {
+                go.GetComponent<Box>().SetColor(HSBColor.FromRGB(Color.black));
+                continue;
+            }
+            go.GetComponent<Box>().SetColor(HSBColor.FromRGB(Color.white));
+        }
+    }
+
+    #endregion
+
+    #region Private Properties
+    private void Update()
+    {
+        foreach (GameObject go in stack)
+            if (go.transform.position.y < -(ScreenHelper.screenHeight / 2) - (boxScale / 2))
+            {
+                Destroy(go);
+                stack.Remove(go);
+                return;
+            }
+
+        if(stack.Count == 0)
+        {
+            onDestroy();
+            Destroy(gameObject);
+        }
+    }
+    private void MakeStack()
+    {
+        boxScale = ScreenHelper.screenWidth / amountOfBoxes;
+
+        if (color.ToColor() == Color.black)
+        {
+            for(int i = 0; i < amountOfBoxes; i++)
+            {
+                if(i == colorPosition)
+                {
+                    stack.Add(Instantiate(box, new Vector2((ScreenHelper.screenWidth / 2 - (boxScale * i)) - (boxScale / 2), (ScreenHelper.screenHeight / 2) + (boxScale / 2)), Quaternion.identity));
+                    stack[i].GetComponent<Box>().Init(boxScale, speed, HSBColor.FromRGB(Color.black), this);
+                    continue;
+                }
+
+                stack.Add(Instantiate(box, new Vector2((ScreenHelper.screenWidth / 2 - (boxScale * i)) - (boxScale / 2), (ScreenHelper.screenHeight / 2) + (boxScale / 2)), Quaternion.identity));
+                stack[i].GetComponent<Box>().Init(boxScale, speed, HSBColor.FromRGB(Color.white), this);
+            }
+            return;
+        }
+
+        HSBColor baseColor = new HSBColor(color.h - colorGap * colorPosition, color.s, color.b);
+
+        for (int i = 0; i < amountOfBoxes; i++)
+        {
+            baseColor = ClampColor(baseColor);
+
+            stack.Add(Instantiate(box, new Vector2((ScreenHelper.screenWidth / 2 - (boxScale * i)) - (boxScale / 2), (ScreenHelper.screenHeight / 2) + (boxScale / 2)), Quaternion.identity));
+            stack[i].GetComponent<Box>().Init(boxScale, speed, baseColor, this);
+
+            baseColor = new HSBColor(baseColor.h + colorGap, baseColor.s, baseColor.b);
+        }
+    }
+    private HSBColor ClampColor(HSBColor color)
+    {
+        if (color.h > 1.0f)
+            color.h -= 1.0f;
+        else if (color.h < 0.0)
+            color.h = 1.0f + color.h;
+        return color;
+    }
+
+    #endregion
+}
